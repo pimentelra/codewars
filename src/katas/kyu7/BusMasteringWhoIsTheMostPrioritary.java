9041d37c4f3735daa44245bf4c431bae69d0f30c
Pimentel <pimentel.ra@gmail.com>
2018-05-20 23:11:09 +0100

    Adapt BusMasteringWhoIsTheMostPrioritary

--- src/katas/kyu7/BusMasteringWhoIsTheMostPrioritary.java
+++ src/katas/kyu7/BusMasteringWhoIsTheMostPrioritary.java
@@ -2,6 +2,6 @@ package katas.kyu7;
 
 public class BusMasteringWhoIsTheMostPrioritary {
     public static String arbitrate(String input, int n) {
-        return input.replaceAll("(?<=(1))((?:(?!\\1).)*)\\1", "$20");
+        return input.replaceAll("(?<=(1))((?:(?!1).)*)1", "$20");
     }
 }
