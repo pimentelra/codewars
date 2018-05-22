95f740df78e3be4a56c0d06ad0c723dccc303765
Pimentel <pimentel.ra@gmail.com>
2018-05-22 23:19:56 +0100

    Add ProductOfMaximumsOfArrayArraySeries2

---
+++ src/katas/kyu7/ProductOfMaximumsOfArrayArraySeries2.java
@@ -0,0 +1,12 @@
+package katas.kyu7;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class ProductOfMaximumsOfArrayArraySeries2 {
+    public static long maxProduct(int[] numbers, int sub_size) {
+        int[] clone = IntStream.of(numbers).boxed().sorted(Comparator.reverseOrder()).mapToInt(i -> i).toArray();
+        return Arrays.stream(clone, 0, sub_size).reduce(1, (a, b) -> a * b);
+    }
+}
