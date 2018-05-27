6ccf11190526fb3d446e03d65fd8ea0cc3145128
Pimentel <pimentel.ra@gmail.com>
2018-05-27 22:50:26 +0100

    Add SQL_with_Pokemon_Damage_Multipliers.sql

---
+++ src/katas/kyu7/SQL_with_Pokemon_Damage_Multipliers.sql
@@ -0,0 +1,4 @@
+SELECT pokemon_name, str * multiplier AS modifiedStrength, element
+FROM pokemon JOIN multipliers ON pokemon.element_id = multipliers.id
+WHERE str * multiplier >= 40
+ORDER BY modifiedStrength DESC;
