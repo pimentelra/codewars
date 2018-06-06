9cf3ad82a655318d6e6d979f522237a49b7e7a59
Pimentel <pimentel.ra@gmail.com>
2018-06-06 23:26:16 +0100

    Add MultiplesOf3Or5

---
+++ src/katas/kyu6/MultiplesOf3Or5.java
@@ -0,0 +1,9 @@
+package katas.kyu6;
+
+import java.util.stream.IntStream;
+
+public class MultiplesOf3Or5 {
+    public int solution(int number) {
+        return IntStream.range(1, number).filter(i -> i % 3 == 0 || i % 5 == 0).sum();
+    }
+}
