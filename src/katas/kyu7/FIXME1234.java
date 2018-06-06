6bc0f94c5d5983b9e6a21de42b06b8cc16d12738
Pimentel <pimentel.ra@gmail.com>
2018-06-06 23:21:58 +0100

    Add FIXME1234

---
+++ src/katas/kyu7/FIXME1234.java
@@ -0,0 +1,40 @@
+package katas.kyu7;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FIXME1234 {
+    private static final Map<String, MyNumber> cache = new HashMap<>();
+
+    static {
+        Arrays.stream(MyNumber.values()).forEach(i -> {});
+    }
+
+    static enum MyNumber {
+        ONE(1, "one", "ichi", "un"),
+        TWO(2, "two", "ni", "deux"),
+        THREE(3, "three", "san", "trois"),
+        FOUR(4, "four", "shi", "quatre");
+
+        private final int val;
+
+        private MyNumber(int val, String... names) {
+            this.val = val;
+            for (final String n : names) cache.put(n, this);
+        }
+
+        public int intValue() {
+            return this.val;
+        }
+
+    }
+
+    static MyNumber getNumber(final String name) {
+        return cache.get(name);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getNumber("shi").intValue());
+    }
+}
