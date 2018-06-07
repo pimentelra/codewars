674bb4f75104c795dbad592198cd380aba4c9ea7
Pimentel <pimentel.ra@gmail.com>
2018-06-07 20:47:11 +0100

    Add RomanNumeralsEncoder

---
+++ src/katas/kyu6/RomanNumeralsEncoder.java
@@ -0,0 +1,20 @@
+package katas.kyu6;
+
+public class RomanNumeralsEncoder {
+    public static String solution(int n) {
+        return String.valueOf(new char[n]).replace('\0', 'I')
+                .replace("IIIII", "V")
+                .replace("IIII", "IV")
+                .replace("VV", "X")
+                .replace("VIV", "IX")
+                .replace("XXXXX", "L")
+                .replace("XXXX", "XL")
+                .replace("LL", "C")
+                .replace("LXL", "XC")
+                .replace("CCCCC", "D")
+                .replace("CCCC", "CD")
+                .replace("DD", "M")
+                .replace("DCD", "CM");
+
+    }
+}
