fb09884c1f36064c20b18e79fb88cdcea011701b
Pimentel <pimentel.ra@gmail.com>
2018-06-08 23:24:29 +0100

    Add ValidPhoneNumber

---
+++ src/katas/kyu6/ValidPhoneNumber.java
@@ -0,0 +1,7 @@
+package katas.kyu6;
+
+public class ValidPhoneNumber {
+    public static boolean validPhoneNumber(String phoneNumber) {
+        return phoneNumber.matches("\\(\\d{3}\\)\\s\\d{3}-\\d{4}");
+    }
+}
