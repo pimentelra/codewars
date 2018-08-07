7e0cd29deb90576249520f984fafa8525a5665d5
Pimentel <pimentel.ra@gmail.com>
2018-08-07 23:08:57 +0100

    Add BindingWithinTheListMonad

---
+++ src/katas/kyu6/BindingWithinTheListMonad.java
@@ -0,0 +1,14 @@
+package katas.kyu6;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class BindingWithinTheListMonad {
+}
+
+class Binder<T, I> {
+    public List<I> bind(List<T> list, Function<T, List<I>> func) {
+        return list.stream().map(func).flatMap(List::stream).collect(Collectors.toList());
+    }
+}
