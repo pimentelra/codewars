fa88e2e75ed9224d468e57f9d5e6d629b5d2ee17
Pimentel <pimentel.ra@gmail.com>
2018-11-28 22:39:07 +0000

    Add OppositesAttract

---
+++ src/katas/kyu8/OppositesAttract.java
@@ -0,0 +1,7 @@
+package katas.kyu8;
+
+public class OppositesAttract {
+    public static boolean isLove(final int flower1, final int flower2) {
+        return flower1 % 2 == 0 && flower2 % 2 != 0 || flower1 % 2 == 1 && flower2 % 2 != 1;
+    }
+}
