1784bae964c72216f5dd578460f7918674232cfa
Pimentel <pimentel.ra@gmail.com>
2018-12-20 21:30:20 +0100

    Add ThinkfulNumberDrillsPixelartPlanning

---
+++ src/katas/kyu8/ThinkfulNumberDrillsPixelartPlanning.java
@@ -0,0 +1,7 @@
+package katas.kyu8;
+
+public class ThinkfulNumberDrillsPixelartPlanning {
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return wallLength % pixelSize == 0;
+    }
+}
