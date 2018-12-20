9e111be5f5d04b967700685e84c5c6fd8abd73e9
Pimentel <pimentel.ra@gmail.com>
2018-12-20 21:18:49 +0100

    Add ExclamationMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftToRight

---
+++ src/katas/kyu8/ExclamationMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftToRight.java
@@ -0,0 +1,8 @@
+package katas.kyu8;
+
+public class ExclamationMarksSeries6RemoveNExclamationMarksInTheSentenceFromLeftToRight {
+    public static String remove(String s, int n) {
+        for (int i = 0; i < n; i++) s = s.replaceFirst("!", "");
+        return s;
+    }
+}
