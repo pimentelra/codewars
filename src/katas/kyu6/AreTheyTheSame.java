9727a6eee3c52ea6cd776a9a658c84d0ee84cce2
Pimentel <pimentel.ra@gmail.com>
2019-05-07 12:38:12 +0100

    Update AreTheyTheSame

--- src/katas/kyu6/AreTheyTheSame.java
+++ src/katas/kyu6/AreTheyTheSame.java
@@ -5,8 +5,7 @@ import java.util.stream.IntStream;
 
 public class AreTheyTheSame {
     public static boolean comp(int[] a, int[] b) {
-            return a != null && b != null && a.length == b.length &&
-                    Arrays.stream(a).noneMatch(i -> IntStream.of(b).noneMatch(x -> Math.pow(i, 2) == Math.abs(x))) &&
-                    Arrays.stream(b).noneMatch(i -> IntStream.of(a).noneMatch(x -> Math.sqrt(i) == Math.abs(x)));
+        return a != null && b != null && a.length == b.length &&
+                IntStream.range(0, b.length).allMatch(i -> Arrays.stream(b).sorted().toArray()[i] == Math.pow(Arrays.stream(a).map(Math::abs).sorted().toArray()[i], 2));
     }
 }
