b548c2c5f5b197aa19f007c6536683623548a8bf
Pimentel <pimentel.ra@gmail.com>
2019-05-19 01:22:36 +0100

    Add YouCantCodeUnderPressure2

---
+++ src/katas/kyu7/YouCantCodeUnderPressure2.java
@@ -0,0 +1,10 @@
+package katas.kyu7;
+
+public class YouCantCodeUnderPressure2 {
+}
+
+class Counter {
+    private int value = 0;
+    public int check() {return value;}
+    public void increment() {value++;}
+}
