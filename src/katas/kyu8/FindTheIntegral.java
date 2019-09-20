2c914ca0d5c7f512603f969a2dc5cea618f146aa
Pimentel <pimentel.ra@gmail.com>
2019-09-20 10:30:00 +0100

    Add FindTheIntegral

---
+++ src/katas/kyu8/FindTheIntegral.java
@@ -0,0 +1,7 @@
+package katas.kyu8;
+
+public class FindTheIntegral {
+    public static String integrate(int coefficient, int exponent) {
+        return coefficient / (exponent + 1) + "x^" + (exponent + 1);
+    }
+}
