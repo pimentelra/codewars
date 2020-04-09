6df28b83c3e2818fcd9e03087c7737f3ee853c34
Pimentel <pimentel.ra@gmail.com>
2020-04-09 21:48:33 +0100

    Add GeometryBasicsDotProductIn3D

---
+++ src/katas/kyu7/GeometryBasicsDotProductIn3D.java
@@ -0,0 +1,7 @@
+package katas.kyu7;
+
+public class GeometryBasicsDotProductIn3D {
+    public static double dotProduct(final Vector3D a, final Vector3D b) {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+}
