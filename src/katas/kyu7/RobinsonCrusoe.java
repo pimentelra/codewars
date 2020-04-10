58faeb8edb0aef3eff43d9a14185e79dc00efcff
Pimentel <pimentel.ra@gmail.com>
2020-04-10 23:06:02 +0100

    Add RobinsonCrusoe

---
+++ src/katas/kyu7/RobinsonCrusoe.java
@@ -0,0 +1,12 @@
+package katas.kyu7;
+
+import java.util.stream.IntStream;
+
+public class RobinsonCrusoe {
+    public static double[] crusoe(int n, double d, int ang, double distmult, double angmult) {
+        return new double[]{
+                IntStream.range(0, n).mapToDouble(i -> (double) i).reduce(0, (a, b) -> a + (d * Math.pow(distmult, b) * Math.cos(Math.toRadians(ang) * Math.pow(angmult, b)))),
+                IntStream.range(0, n).mapToDouble(i -> (double) i).reduce(0, (a, b) -> a + (d * Math.pow(distmult, b) * Math.sin(Math.toRadians(ang) * Math.pow(angmult, b))))
+        };
+    }
+}
