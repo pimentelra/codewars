06ab208a00ef397a646dea60948538e398f5c5cd
Pimentel <pimentel.ra@gmail.com>
2021-03-18 15:07:43 +0000

    Add DifferenceOfVolumesOfCuboids

---
+++ src/katas/kyu8/DifferenceOfVolumesOfCuboids.java
@@ -0,0 +1,13 @@
+package katas.kyu8;
+
+import java.util.Arrays;
+
+public class DifferenceOfVolumesOfCuboids {
+
+  public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
+    return Math.abs(
+        Arrays.stream(firstCuboid).reduce(1, (a, b) -> a * b) -
+            Arrays.stream(secondCuboid).reduce(1, (a, b) -> a * b)
+    );
+  }
+}
